5225ae5ff67dc9c9127fc8655c4a578c303ddc0b
bearname <ya.mikushoff@gmail.com>
2021-07-18 11:48:23 +0300

    fix

--- config.js
+++ config.js
@@ -2,15 +2,13 @@ const env = process.env;
 const pg = require('pg');
 pg.defaults.ssl = true;
 
-exports.config = {
-    db: new pg.Pool({
-        host: env.DB_HOST || 'localhost',
-        port: env.DB_PORT || '5432',
-        user: env.DB_USER || 'postgres',
-        password: env.DB_PASSWORD || 'postgres',
-        database: env.DB_NAME || 'wildberries',
-        ssl: {
-            rejectUnauthorized: false
-        }
-    })
-}
+exports.pool = new pg.Pool({
+    host: env.DB_HOST || 'localhost',
+    port: env.DB_PORT || '5432',
+    user: env.DB_USER || 'postgres',
+    password: env.DB_PASSWORD || 'postgres',
+    database: env.DB_NAME || 'wildberries',
+    ssl: {
+        rejectUnauthorized: false
+    }
+})
